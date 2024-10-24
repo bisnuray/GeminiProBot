ee48540f07491ceb8a801a898b4593d58a5741e6
Bisnu Ray <79582318+bisnuray@users.noreply.github.com>
2024-10-24 23:55:25 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Copyright (C) @TheSmartBisnu
+# Channel: https://t.me/itsSmartDev
+
+# Pyrogram setup
+API_ID = "12345678"  # Replace this API ID with your actual API ID
+API_HASH = "XXXXXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "XXXXXXXXXXXXXXXXXXX"  # Replace this BOT_TOKEN
+
+# Google Api Key
+GOOGLE_API_KEY = "XXXXXXXXXXXXXXXXXXXX"  # Replace this Google Api Key
+MODEL_NAME = "gemini-1.5-flash" # Don't Change this model
